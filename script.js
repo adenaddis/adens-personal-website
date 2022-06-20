040656b8a5ef960b2601d152ea123cbd6ba7b12e
Aden Addis <adenwonde56@gmail.com>
2022-06-20 12:27:06 -0400

    add script.js, connect to index.html twice

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+const p2 = document.createElement("p2");
+p2.textContent = "This is a paragraph for h2 created by JavaScript";
+document.querySelector("body").appendChild(p2);
